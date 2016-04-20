3f9be0742d2d0ca7a9955f34e38a000163072ce9
Zoltan Altfatter <altfatterz@gmail.com>
2016-04-20 12:24:35 +0200

    add event handler and logging

--- src/main/java/com/trifork/axon/quickstart/ToDoEventHandler.java
+++ src/main/java/com/trifork/axon/quickstart/ToDoEventHandler.java
@@ -1,4 +1,16 @@
 package com.trifork.axon.quickstart;
 
+import org.axonframework.eventhandling.annotation.EventHandler;
+
 public class ToDoEventHandler {
+
+    @EventHandler
+    public void handle(ToDoItemCreatedEvent event) {
+        System.out.println("We've got something to do: " + event.getDescription() + " (" + event.getTodoId() + ")");
+    }
+
+    @EventHandler
+    public void handle(ToDoItemCompletedEvent event) {
+        System.out.println("We've completed a task: " + event.getTodoId());
+    }
 }
